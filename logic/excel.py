0823cd6d7a91a63967afced44b77f8d5bf0c39f3
Artem Holodyr <artemmholodyr@gmail.com>
2023-02-09 22:17:30 +0200

    version 1.0.1

--- logic/excel.py
+++ logic/excel.py
@@ -4,4 +4,6 @@ from parse_cars import get_cars_parsed, FIELDS, PAGES
 data = get_cars_parsed(pages=PAGES)
 
 new_df = pd.DataFrame(data=data, columns=FIELDS)
-new_df.to_csv("../info_cars2.csv", sep=',')
+
+csv_name = 'info_cars2.csv'
+new_df.to_csv(f"../{csv_name}", sep=',')
